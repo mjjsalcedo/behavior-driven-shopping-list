b521b0fa8dab9b8783e809d9a6644ff71035a0df
Mariel Salcedo <Mariel Salcedo>
2017-07-05 19:19:45 -1000

    made first test, and it passes

--- Shopping-List/js/shopping_list_test.js
+++ Shopping-List/js/shopping_list_test.js
@@ -1,13 +1,22 @@
-var expect = require('chai').expect;
+var expect = chai.expect;
+var should = chai.should();
+
 describe('ShoppingListItem', function(){
 
-    class ShoppingListItem {
-
-    }
-
-  var item = new ShoppingListItem();
+  var spoon = ShoppingListItem;
 
   it('should be a function', function(){
-    expect(ShoppingListItem).to.be.a('function');
+    expect(spoon).to.be.a('function')
   });
+
+  /*it('should have a name', function(){
+    var bob = new Human('Bob')
+    expect(bob.name).to.equal('Bob')
+    expect(bob.name).to.not.equal('Alice')
+  });
+
+  it('should have a method sayHello()', function(){
+    var bob = new Human('Bob')
+    expect(bob.sayHello()).to.equal('Hello');
+  })*/
 });
